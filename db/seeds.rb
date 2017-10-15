2490ca2fc9b1dfeb47fb94de62706bde9bc4bc87
Chris Marshall <cmarshall10450@gmail.com>
2017-10-15 19:07:45 +0100

    CRUD methods for Film working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,25 @@ customer_1 = Customer.new({
 	'name' => 'Chris Marshall',
 	'funds' => 1000
 })
-
 customer_1.save
+
+customer_2 = Customer.new({
+	'name' => 'Darren Kerr',
+	'funds' => 500
+})
+customer_2.save
+
+film_1 = Film.new({
+	'title' => 'The Usual Supects',
+	'price' => 25
+})
+film_1.save
+
+film_2 = Film.new({
+	'title' => 'The Longest Yard',
+	'price' => 15
+})
+film_2.save
 
 binding.pry
 nil
--- models/Film.rb
+++ models/Film.rb
@@ -7,15 +7,29 @@ class Film
 	end
 
 	def save
+		sql = 'INSERT INTO films (
+  					title,
+  					price
+					)
+					VALUES (
+						$1, $2
+					)
+					RETURNING id;'
 
+		values = [@title, @price]
+		@id = SQLRunner.run(sql, values)[0]['id'].to_i
 	end
 
 	def update
-
+		sql = 'UPDATE films SET (title, price) = ($1, $2) WHERE id = $3;'
+		values = [@title, @price, @id]
+		SQLRunner.run(sql, values)
 	end
 
 	def delete
-
+		sql = 'DELETE FROM films WHERE id = $1'
+		values = [@id]
+		SQLRunner.run(sql, values)
 	end
 
 	def customers
